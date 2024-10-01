68ef70a554ebb5e262853f747f9a643589c9b195
rekhatetala <151903598+rekhatetala@users.noreply.github.com>
2024-10-01 12:04:35 +0530

    Create linear search.py

---
+++ linear search.py
@@ -0,0 +1,12 @@
+def linear_search(arr, target):
+    for index in range(len(arr)):
+        if arr[index] == target:
+            return index
+    return -1
+arr = [10, 23, 45, 70, 11, 15]
+target = 70
+result = linear_search(arr, target)
+if result != -1:
+    print(f"Element found at index: {result}")
+else:
+    print("Element not found in the array")
